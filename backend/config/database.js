00c83dd83f8db4caf9afb1a710dea0f32fb58b70
DJ Meph <norm.witte@gmail.com>
2022-03-03 05:30:54 +0000

    Enable MySQL database and Page content type

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,19 @@
-const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  const sslSelf = env.bool('DATABASE_SSL_SELF', false);
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'mysql',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: sslSelf ? {
+          rejectUnauthorized: sslSelf, // For self-signed certificates
+        } : false,
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
